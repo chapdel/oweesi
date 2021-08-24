840593a51ffc21d6c2fae0006e1fa8e9bffad525
chapdel <drop@chapdel.me>
2021-08-24 21:45:09 +0100

    Added id to guarded

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email',
+    protected $guarded = [
+        'id'
     ];
 
     /**
