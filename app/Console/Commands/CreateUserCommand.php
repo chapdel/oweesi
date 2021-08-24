8292af2304a0c67762eb74fb6608a9982c3a2dfe
chapdel <drop@chapdel.me>
2021-08-24 20:57:26 +0100

    Added create user command

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create new user';
+
+    protected $currencies = [];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info("Creating new user");
+
+        $name = $this->ask("Name", "Forex");
+        $email = $this->ask("Email Address", "support@forexapi.world");
+        $password = $this->ask("Password");
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info("API Key Genrated: $user->uid");
+    }
+}
